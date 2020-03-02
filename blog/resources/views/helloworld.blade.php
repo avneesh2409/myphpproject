790fe19f8490d861c23c76e3253535ea951372a1
= <=>
2020-03-02 08:08:38 +0530

    hello world route added

---
+++ blog/resources/views/helloworld.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Hello World MVC architecture</title>
+    </head>
+    <body>
+      <div>
+        <h1>yeh we got it</h1>
+      </div>
+    </body>
+</html>
